cdc764b6b081413f67cc36e0fd85b7bd190274f9
Arelhiasj <asj@soluciones4g.com>
2017-06-28 17:42:13 +0000

    [REF]: Adding Web_services

---
+++ ws_test.py
@@ -0,0 +1,23 @@
+import functools
+import xmlrpclib
+HOST = 'localhost'
+PORT = 8069
+DB = 'odoo_curso'
+USER = 'asj@soluciones4g.com'
+PASS = '123456789'
+ROOT = 'http://%s:%d/xmlrpc/' % (HOST,PORT)
+
+# 1. Login
+uid = xmlrpclib.ServerProxy(ROOT + 'common').login(DB,USER,PASS)
+print "Logged in as %s (uid:%d)" % (USER,uid)
+
+call = functools.partial(
+    xmlrpclib.ServerProxy(ROOT + 'object').execute,
+    DB, uid, PASS)
+
+# 2. Read the sessions
+model = 'openacademy.session'
+domain = []
+method_name = 'search_read'
+sessions = call(model, method_name, domain, ['name','seats'])
+print "sessions", sessions
